34cf103bb86ececdf15fff92624435752ee6cfcf
Mike Kurkin <mike@kurkin.net>
2022-06-06 23:08:29 +0300

    Refactoring

--- auctions/models.py
+++ auctions/models.py
@@ -17,10 +17,13 @@ class Listing(models.Model):
     title = models.CharField(max_length=64)
     description = models.TextField(max_length=1000)
     creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
-    active = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
     st_bid = models.FloatField(verbose_name="Starting bid")
     image_url = models.URLField(max_length=200, null=True, blank=True)
     category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL, related_name="listings")
 
     def __str__(self):
-        return f"{self.id}: {self.title} by {self.creator}"
+        s = f"{self.id}: {self.title} by {self.creator}"
+        if not self.is_active:
+            s += " (Inactive)"
+        return s
