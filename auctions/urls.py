76a74bb6811091a64037d94900a796eba66621bc
Mike Kurkin <mike@kurkin.net>
2022-06-06 23:20:41 +0300

    View and create listings

--- auctions/urls.py
+++ auctions/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("listing/<int:listing_id>", views.listing_show, name="listing"),
+    path("listing/new", views.listing_new, name="listing_new"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register")
--- auctions/views.py
+++ auctions/views.py
@@ -1,14 +1,52 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.db import IntegrityError
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import User
+from .models import User, Listing, Category
 
 
 def index(request):
-    return render(request, "auctions/index.html")
+    active_listings = Listing.objects.filter(is_active=True)
+    return render(request, "auctions/index.html", {
+        "listings": active_listings
+    })
+
+
+def listing_show(request, listing_id):
+    listing = Listing.objects.get(pk=listing_id)
+    return render(request, "auctions/listing.html", {
+        "listing": listing
+    })
+
+
+@login_required
+def listing_new(request):
+    if request.method == "POST":
+        title = request.POST.get("title")
+        description = request.POST.get("description")
+        st_bid = float(request.POST.get("st_bid"))
+        image_url = request.POST.get("image_url")
+        if image_url == "":
+            image_url = None
+        category = request.POST.get("category")
+        if category is not None:
+            category = int(category)
+        creator = request.user
+
+        listing = Listing(title=title, description=description, st_bid=st_bid, image_url=image_url,
+                          category=category, creator=creator, is_active=True)
+        listing.save()
+        return HttpResponseRedirect(reverse("listing", args=(listing.pk,)))
+
+    listing = Listing()
+    categories = Category.objects.all()
+    return render(request, "auctions/listing_new.html", {
+        "listing": listing,
+        "categories": categories
+    })
 
 
 def login_view(request):
