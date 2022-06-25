3e60eb8e9526fe1f5196c34bf2f8e029e973b62a
Mike Kurkin <mike@kurkin.net>
2022-06-26 01:24:59 +0300

    Refactoring

--- project3/mail/mail/static/mail/inbox.js
+++ project3/mail/mail/static/mail/inbox.js
@@ -12,7 +12,6 @@ document.addEventListener('DOMContentLoaded', function() {
   // By default, load the inbox
   let current_mailbox = 'inbox';
   load_mailbox(current_mailbox);
-  // load_email(1);
 });
 
 function compose_email(reply_email=null) {
@@ -24,17 +23,16 @@ function compose_email(reply_email=null) {
   }
 
   // Show compose view and hide other views
-  // document.querySelector('#emails-view').style.display = 'none';
-  // document.querySelector('#compose-view').style.display = 'block';
   document.querySelector('#emails-view').setAttribute('hidden', true);
   document.querySelector('#compose-view').removeAttribute('hidden');
-  console.log(reply_email)
+
   if (reply_email === null) {
     // Clear out composition fields
     document.querySelector('#compose-recipients').value = '';
     document.querySelector('#compose-subject').value = '';
     document.querySelector('#compose-body').value = '';
   } else {
+    // Pre-fill fields from reply email
     document.querySelector('#compose-recipients').value = reply_email.sender;
     if (reply_email.subject.slice(0,3).toLowerCase() != 're:') {
       document.querySelector('#compose-subject').value = `Re: ${reply_email.subject}`;
@@ -49,7 +47,6 @@ ${reply_email.body}`;
 
     document.querySelector('#compose-body').focus();
   }
-  
 
   // Clear out error alert
   const formAlert = document.querySelector('#form_alert');
@@ -59,10 +56,12 @@ ${reply_email.body}`;
 }
 
 function post_email() {
+  // Get values from fields
   const recipients = document.querySelector('#compose-recipients').value
   const subject = document.querySelector('#compose-subject').value
   const body = document.querySelector('#compose-body').value
   
+  // Send request
   fetch('/emails', {
     method: 'POST', 
     body: JSON.stringify({
@@ -73,8 +72,8 @@ function post_email() {
   })
   .then(response => response.json())
   .then(result => {
-    console.log(result);
     if (result.error != undefined) {
+      // Present error if API request has one
       const alertEl = document.createElement('div')
       alertEl.id = 'form_alert'
       alertEl.classList.add('alert')
@@ -86,11 +85,112 @@ function post_email() {
       }
       document.querySelector('#compose-form').prepend(alertEl);
     } else {
+      // If no error show sent inbox
       load_mailbox('sent');
     } 
   })
   
   return false;
+}
+
+function load_mailbox(mailbox) {
+  // Remove all active button attributes and set for the current mailbox
+  document.querySelectorAll('.container button.active').forEach(el => {
+    el.classList.remove('active');
+  })
+  if (document.querySelector(`#${mailbox}`) != null) {
+    document.querySelector(`#${mailbox}`).classList.add('active')
+  }
+  
+  // Show the mailbox and hide other views
+  document.querySelector('#emails-view').removeAttribute('hidden');
+  document.querySelector('#compose-view').setAttribute('hidden', true);
+
+  // Show the mailbox name with listener to re-load mailbox
+  const mailboxTitle = document.createElement('div')
+  mailboxTitle.id = 'mailbox-title'
+  mailboxTitle.innerHTML = `<a class="text-dark text-decoration-none" href="#"><h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3></a>`;
+  mailboxTitle.addEventListener('click', () => {load_mailbox(mailbox)})
+  if (document.querySelector('#mailbox-title') === null) {
+    document.querySelector('#emails-view').appendChild(mailboxTitle)
+  } else {
+    document.querySelector('#mailbox-title').replaceWith(mailboxTitle)
+  }
+
+  current_mailbox = mailbox;
+
+  // Get the list of e-mails
+  fetch(`/emails/${mailbox}`)
+  .then(response => response.json())
+  .then(emails => {
+    if (document.querySelector('#mailbox-wrapper') === null) {
+      document.querySelector('#emails-view').appendChild(mailbox_wrapper_el())
+    } else {
+      document.querySelector('#mailbox-wrapper').replaceWith(mailbox_wrapper_el())
+    }
+
+    // Show list of e-mails
+    if (emails.length > 0) {
+      emails.forEach(email => {
+        document.querySelector('#mailbox').appendChild(list_card_el(email));
+      });
+    };
+  })
+  .catch(error => console.log(error));
+}
+
+// Set read property to true
+function load_email(id) {
+  fetch(`/emails/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+        read: true
+    })
+  })
+  .then(() => {
+    // Load the email
+    fetch(`/emails/${id}`)
+    .then(response => response.json())
+    .then(email => {
+      document.querySelector(`#email${email.id}`).replaceWith(list_card_el(email));
+  
+      // Remove all active card attributes and set for the current card
+      document.querySelectorAll('#mailbox button.active').forEach(el => {
+        el.classList.remove('active');
+      })
+      document.querySelector(`#email${email.id}`).classList.add('active');
+
+      
+      // Hide mailbox column on small screens
+      document.querySelector(`#mailbox`).classList.add('d-none');
+
+      // Show email view
+      const emailView = document.querySelector('#email_view');
+      if (emailView != null) {
+        emailView.replaceWith(email_view_el(email));
+      } else {
+        document.querySelector('#mailbox-wrapper').appendChild(email_view_el(email));
+      }
+    });
+  })
+  .catch(error => console.log(error));
+}
+
+function set_archived(email_id, value=true) {
+  // Send request to set archived property true or false
+  fetch(`/emails/${email_id}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+        archived: value
+    })
+  })
+  .then(() => {
+    load_mailbox('inbox');
+    if (!value) {
+      load_email(email_id);
+    }
+  })
+  .catch(error => console.log(error));
 }
 
 // Returns email list card element
@@ -164,111 +264,7 @@ function email_view_el(email) {
   return emailView
 } 
 
-function load_mailbox(mailbox) {
-  document.querySelectorAll('.container button.active').forEach(el => {
-    el.classList.remove('active');
-  })
-  if (document.querySelector(`#${mailbox}`) != null) {
-    document.querySelector(`#${mailbox}`).classList.add('active')
-  }
-  
-  // Show the mailbox and hide other views
-  // document.querySelector('#emails-view').style.display = 'block';
-  // document.querySelector('#compose-view').style.display = 'none';
-  document.querySelector('#emails-view').removeAttribute('hidden');
-  document.querySelector('#compose-view').setAttribute('hidden', true);
-
-  // Show the mailbox name
-  const mailboxTitle = document.createElement('div')
-  mailboxTitle.id = 'mailbox-title'
-  mailboxTitle.innerHTML = `<a class="text-dark text-decoration-none" href="#"><h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3></a>`;
-  mailboxTitle.addEventListener('click', () => {
-    load_mailbox(mailbox)
-  })
-  if (document.querySelector('#mailbox-title') === null) {
-    document.querySelector('#emails-view').appendChild(mailboxTitle)
-  } else {
-    document.querySelector('#mailbox-title').replaceWith(mailboxTitle)
-  }
-  // document.querySelector('#emails-view').innerHTML = `<a class="text-dark text-decoration-none" href="#"><h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3></a>`;
-  // document.querySelector('#mailbox-title').addEventListener('click', () => {
-  //   load_mailbox(mailbox);
-  // })
-
-  current_mailbox = mailbox;
-  // Get the list of e-mails
-  fetch(`/emails/${mailbox}`)
-  .then(response => response.json())
-  .then(emails => {
-    console.log(emails.length);
-    // document.querySelector('#emails-view').appendChild(mailbox_wrapper_el());
-    if (document.querySelector('#mailbox-wrapper') === null) {
-      document.querySelector('#emails-view').appendChild(mailbox_wrapper_el())
-    } else {
-      document.querySelector('#mailbox-wrapper').replaceWith(mailbox_wrapper_el())
-    }
-
-    // Show list of e-mails
-    if (emails.length > 0) {
-      
-      
-
-      emails.forEach(email => {
-        document.querySelector('#mailbox').appendChild(list_card_el(email));
-      });
-    };
-  })
-  .catch(error => console.log(error));
-}
-
-function load_email(id) {
-  console.log(`loading ${id}`);
-  fetch(`/emails/${id}`, {
-    method: 'PUT',
-    body: JSON.stringify({
-        read: true
-    })
-  })
-  .then(() => {
-    fetch(`/emails/${id}`)
-    .then(response => response.json())
-    .then(email => {
-      document.querySelector(`#email${email.id}`).replaceWith(list_card_el(email));
-  
-      // Remove all active attributes and set for the current card
-      document.querySelectorAll('#mailbox button.active').forEach(el => {
-        el.classList.remove('active');
-      })
-      document.querySelector(`#email${email.id}`).classList.add('active');
-
-      let emailView = document.querySelector('#email_view');
-      if (emailView != null) {
-        emailView.remove();
-      }
-      document.querySelector(`#mailbox`).classList.add('d-none');
-      document.querySelector('#mailbox-wrapper').appendChild(email_view_el(email));
-    
-    });
-  })
-  .catch(error => console.log(error));
-}
-
-function set_archived(email_id, value=true) {
-  fetch(`/emails/${email_id}`, {
-    method: 'PUT',
-    body: JSON.stringify({
-        archived: value
-    })
-  })
-  .then(() => {
-    load_mailbox('inbox');
-    if (!value) {
-      load_email(email_id);
-    }
-  })
-  .catch(error => console.log(error));
-}
-
+// Returns mailbox wrapper structure element
 function mailbox_wrapper_el() {
   const mailboxWrapper = document.createElement('div')
   mailboxWrapper.id = 'mailbox-wrapper';
