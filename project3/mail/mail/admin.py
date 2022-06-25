3cf0924ce093380d84d9d8951082a900471cc911
Mike Kurkin <mike@kurkin.net>
2022-06-26 00:12:16 +0300

    Admin layout

--- project3/mail/mail/admin.py
+++ project3/mail/mail/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Email, User
+
+
+class EmailAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'user', 'sender', 'subject', 'timestamp')
+
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Email, EmailAdmin)
