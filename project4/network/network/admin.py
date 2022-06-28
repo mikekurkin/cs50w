a8375579e91acc023146ca1f4222e30e2f189758
Mike Kurkin <mike@kurkin.net>
2022-06-28 10:00:58 +0300

    Models

--- project4/network/network/admin.py
+++ project4/network/network/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import User, Post, FollowRelationship, Like
+
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Post)
+admin.site.register(FollowRelationship)
+admin.site.register(Like)
--- project4/network/network/models.py
+++ project4/network/network/models.py
@@ -3,4 +3,31 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    followers = models.ManyToManyField(
+        "User",
+        through="FollowRelationship",
+        through_fields=("follows", "follower"),
+        related_name="following")
+
+
+class Post(models.Model):
+    author = models.ForeignKey("User", on_delete=models.PROTECT, related_name="posts")
+    contents = models.TextField(max_length=1000)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    liked_by = models.ManyToManyField(
+        "User",
+        through="Like",
+        related_name="liked_posts",
+        editable=True)
+
+
+class FollowRelationship(models.Model):
+    follower = models.ForeignKey("User", on_delete=models.CASCADE, related_name="following_relationships")
+    follows = models.ForeignKey("User", on_delete=models.CASCADE, related_name="follows_relationships")
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+
+class Like(models.Model):
+    user = models.ForeignKey("User", on_delete=models.CASCADE, related_name="likes")
+    posts = models.ForeignKey("Post", on_delete=models.CASCADE, related_name="likes")
+    timestamp = models.DateTimeField(auto_now_add=True)
